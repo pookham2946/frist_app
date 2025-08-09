f17113b43e50c81b3d3c10e21a281da9742b1b0a
pookham2946 <pookham_i@su.ac.th>
2025-08-09 16:43:09 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyWidget()
+    );
+  }
+}
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: false,
+        backgroundColor: const Color.fromARGB(255, 248, 247, 248),
+      ),
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 300,
+              width: 120,
+              decoration: BoxDecoration(color: Colors.black,
+              borderRadius: BorderRadius.circular(20)
+              ),
+            ),
+            Positioned(
+              top: 20,
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 110,
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 200,
+              child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
